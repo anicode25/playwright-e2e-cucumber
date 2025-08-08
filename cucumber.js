4da168290573a1b9bc4017ddf8a5258d6a3b9b29
anicode25 <aniket.ghosh@codelogicx.com>
2025-08-08 15:21:06 +0530

    added: multiple cucumber HTML reporter

--- cucumber.js
+++ cucumber.js
@@ -3,7 +3,7 @@ module.exports = {
     require: ['tests/steps/**/*.ts', 'tests/support/**/*.ts'],
     requireModule: ['ts-node/register'],
     paths: ['tests/features/**/*.feature'],
-    format: ['progress'],
+    format: ['json:cucumber-report/report.json','progress'],
     publishQuiet: true
   }
 };
--- generate-report.js
+++ generate-report.js
@@ -1,11 +1,27 @@
-const reporter = require('cucumber-html-reporter');
+const report = require('multiple-cucumber-html-reporter');
 
-const options = {
-  theme: 'bootstrap',
-  jsonFile: 'cucumber-report/report.json',
-  output: 'cucumber-report/report.html',
-  reportSuiteAsScenarios: true,
-  launchReport: true, // <--- opens in browser automatically
-};
+report.generate({
+  jsonDir: 'cucumber-report', // The folder containing the Cucumber JSON
+  reportPath: 'cucumber-report/html', // The output folder for HTML
+  metadata: {
+    browser: {
+      name: 'chrome',
+      version: '126'
+    },
+    device: 'Local test machine',
+    platform: {
+      name: 'windows',
+      version: '10'
+    }
+  },
+  customData: {
+    title: 'Run info',
+    data: [
+      { label: 'Project', value: 'Swag Labs_e2e' },
+      { label: 'Release', value: '1.0.0' },
+      { label: 'Execution Start Time', value: new Date().toLocaleString() },
+      { label: 'Execution End Time', value: new Date().toLocaleString() }
+    ]
+  }
+});
 
-reporter.generate(options);
